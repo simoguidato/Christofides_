c3576039e8832cf41ab27bc325052082a19afc61
simoguidato <simoguidato@libero.it>
2024-09-14 13:10:28 +0200

    Initial commit

---
+++ Main.py
@@ -0,0 +1,146 @@
+import tsplib95
+import networkx as nx
+import time
+from networkx.algorithms import matching
+from itertools import combinations
+
+start_time = time.process_time()
+
+
+def load_tsp_problem(filename):
+    return tsplib95.load(filename)
+
+def christofides_tsp(input_graph):
+    # Step 1: Calcareous il Minimum Spanning Tree (MST)
+    mst = nx.minimum_spanning_tree(input_graph)
+
+    # Step 2: Trovare i vertici con grado dispari nel MST
+    odd_degree_nodes = [v for v, degree in mst.degree() if degree % 2 == 1]
+
+    # Step 3: Creare il sottografo indotto dai vertici con grado dispari
+    subgraph = input_graph.subgraph(odd_degree_nodes)
+
+    # Step 4: Trovare il Matching perfetto con costo minimo
+    matching_edges = matching.min_weight_matching(subgraph, maxcardinality=True)
+
+    # Step 5: Unire gli archi del Matching al MST per ottenere un multigrafo Euleriano
+    multigraph = nx.MultiGraph(mst)
+    multigraph.add_edges_from(matching_edges)
+
+    # Step 6: Trovare un ciclo Euleriano nel multigrafo
+    eulerian_circuit = list(nx.eulerian_circuit(multigraph))
+
+    # Step 7: Convertire il ciclo Euleriano in un ciclo Hamiltoniano (rimuovere i duplicati)
+    tspath = []
+    visited = set()
+    for u, v in eulerian_circuit:
+        if u not in visited:
+            tspath.append(u)
+            visited.add(u)
+        if v not in visited:
+            tspath.append(v)
+            visited.add(v)
+
+
+    tspath.append(tspath[0])
+
+    return tspath
+
+
+# Calcola il costo del ciclo Hamiltoniano
+def calcola_costo(h_graph, path):
+    costo = 0
+    for i in range(len(path) - 1):
+        costo += h_graph[path[i]][path[i + 1]]['weight']
+    return costo
+
+
+def create_graph(tsp_problem):
+    new_graph = nx.Graph()
+    nodes = list(tsp_problem.get_nodes())
+    for u, v in combinations(nodes, 2):
+        new_graph.add_edge(u, v, weight=problem.get_weight(u, v))
+    return new_graph
+
+
+#ottengo valore soluzione ottima
+def parse_file(filename):
+    risultati = {}
+    with open(filename, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if line:
+                parts = line.split(':')
+                if len(parts) == 2:
+                    key = parts[0].strip()
+                    value_str = parts[1].strip()
+                    try:
+                        value = int(value_str.split()[0])  # Considera solo la parte numerica
+                        risultati[key] = value
+                    except ValueError:
+                        print(f"Valore non valido per la chiave '{key}': {value_str}")
+    return risultati
+#DEVI DISTINGUERE TSP EUCLIDEO (EDGE_WEIGHT_TYPE : EUC_2D) DAL TIPO DELLE ALTRE ISTANZE PERCHÉ QUI SAPPIAMO CHE È GARANTITO IL RAPORTO DI APPROSSIMAZIONE
+def is_euclidean_tsp(prob):
+
+    nodes = list(prob.get_nodes())
+    for i in range(len(nodes)):
+        for j in range(i + 1, len(nodes)):
+            for k in range(j + 1, len(nodes)):
+                a, b, c = nodes[i], nodes[j], nodes[k]
+                d_ab = problem.get_weight(a, b)
+                d_ac = problem.get_weight(a, c)
+                d_bc = problem.get_weight(b, c)
+
+                # Verifica la disuguaglianza triangolare
+                if d_ab > d_ac + d_bc or d_ac > d_ab + d_bc or d_bc > d_ab + d_ac:
+                    print(f"La disuguaglianza triangolare è violata per i nodi {a}, {b}, {c}")
+                    return False
+    return True
+
+#da inserire nel report se è metrico o meno
+
+def cerca_valore(list, chiave):
+    if chiave in list:
+        print(f"Il valore corrispondente a '{chiave}' è: {list[chiave]}")
+        if is_euclidean_tsp(problem):
+            print("è euclideo")
+        else:
+            print("non è euclideo")
+            if tsp_cost/list[chiave] > 3 / 2:
+                print(f"non lo rispetta '{tsp_cost / list[chiave]}'.")
+            else:
+                print(f"Il problema TSP rispetta il rapporto di approssimazione,{tsp_cost / list[chiave]}.")
+        error=(tsp_cost - list[chiave])/tsp_cost
+        report('/home/simo/PycharmProjects/Christofides/Report',tsp_cost,tsp_cost / list[chiave],error,is_euclidean_tsp(problem))
+        print(f"Errore: {error}")
+
+    else:
+        return f"La chiave '{chiave}' non è stata trovata."
+
+def report(file, tsp_value, approx, err,bool):
+    with open(file, 'a') as file:
+        file.write(f"{chiave_da_cercare}: Costo del percorso approssimato TSP: {tsp_value}, Rapporto di approssimazione: {approx}, Errore: {err},Is Euclidean: {bool}\n")
+
+
+tsp_file = '/home/simo/PycharmProjects/Christofides/TSP/brd14051.tsp'
+problem = load_tsp_problem(tsp_file)
+graph = create_graph(problem)
+tsp_path = christofides_tsp(graph)
+tsp_cost = calcola_costo(graph, tsp_path)
+chiave_da_cercare = 'brd14051'
+print("Il percorso approssimato TSP è:")
+print(tsp_path)
+print(f"Il costo del percorso approssimato TSP è: {tsp_cost}")
+filepath = '/home/simo/PycharmProjects/Christofides/SolutionsTSP'
+dizionario = parse_file(filepath)
+
+
+risultato = cerca_valore(dizionario, chiave_da_cercare)
+print(risultato)
+temp=time.process_time() - start_time
+print(f"tempo totale: {temp}")
+
+
+
+
